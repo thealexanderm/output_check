8d7a9c2551e86185c018766d0e7c8bb6da5d0c2b
Alexander Mulder <thealexandermulder@gmail.com>
2025-11-21 17:23:31 -0500

    Update and rename outputCheck.py to outputcheck.py

--- outputcheck.py
+++ outputcheck.py
@@ -1,22 +1,22 @@
-def outputCheck(correctOutput, myOutput):
+def output_check(correct_output, my_output):
     if(correctOutput!=myOutput):
         return False
 
 
 
 if __name__ == '__main__':
-    with open("correctOutput.txt", "r") as f:
+    with open("correct_output.txt", "r") as f:
         lines = f.readlines()
         last = lines[-1]
-    with open("correctOutput.txt", "r") as f1, open("myOutput.txt", "r") as f2:
+    with open("correct_output.txt", "r") as f1, open("my_output.txt", "r") as f2:
         for x, y in zip(f1, f2):
-            if(outputCheck(x,y)==False):
+            if(output_check(x,y)==False):
                 print("There's an error :(")
                 print(f"Your output is: {y}")
                 print(f"The correct output is: {x}")
                 break
             elif(x==last):
-                outputCheck(x,y)
+                output_check(x,y)
                 print("Yay! No errors!")
             else:
-                outputCheck(x, y)
+                output_check(x, y)
